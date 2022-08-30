d9be4a963591200a1fa1a0b31ff482409b2eb9a8
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-08-30 12:36:27 -0500

    FEATURE/LOGIN user login basic grant

--- functions/auth/controller.js
+++ functions/auth/controller.js
@@ -16,4 +16,23 @@ const signUpUser = async (req, res) => {
   }
 };
 
-module.exports = { signUpUser };
+const loginUser = async (req, res) => {
+  const { user } = req.body;
+
+  try {
+    const findUserResponse = await findUser(user.userName);
+    if (findUserResponse.Items.length > 0) {
+      if (user.password === findUserResponse.Items[0].password) {
+        res.status(200).json({ message: "User access granted" });
+      } else {
+        res.status(401).json({ message: "Incorrect password" });
+      }
+    } else {
+      res.status(404).json({ message: "User is not on our system" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { signUpUser, loginUser };
--- functions/auth/router.js
+++ functions/auth/router.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { signUpUser } = require("./controller");
+const { signUpUser, loginUser } = require("./controller");
 
 const router = Router();
 
 router.post("/signup", signUpUser);
+router.post("/login", loginUser);
 
 module.exports = router;
